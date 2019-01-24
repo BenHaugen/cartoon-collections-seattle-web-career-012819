f882482563acae461a32da1d17f637c741aef4f9
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-24 01:25:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese_types|
-    cheese_types.include?
+    cheese_types.include?(array)
     return array
 end
 end
