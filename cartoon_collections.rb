5ad07565e33e092544b3982d04f65214d3a0a55e
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-24 00:57:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,11 +9,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.each do |word|
-    if word.length > 4 
-      return true
-end  
-      return false
+  array.any? |words|
 end
 end
 def find_the_cheese# code an argument here
